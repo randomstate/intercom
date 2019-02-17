05f197e50861b95ad5b17b7c08efbaf73c9f15ee
Connor Forsyth <connorimrie@outlook.com>
2019-02-17 11:48:33 +0000

    Add events fake spy

--- src/Fakes/IntercomClient.php
+++ src/Fakes/IntercomClient.php
@@ -4,6 +4,7 @@
 namespace Contingens\Intercom\Fakes;
 
 use Intercom\IntercomCompanies;
+use Intercom\IntercomEvents;
 use Intercom\IntercomUsers;
 use Mockery as m;
 
@@ -11,10 +12,12 @@ class IntercomClient
 {
     public $users;
     public $companies;
+    public $events;
 
     public function __construct()
     {
         $this->users = m::spy(IntercomUsers::class);
         $this->companies = m::spy(IntercomCompanies::class);
+        $this->events = m::spy(IntercomEvents::class);
     }
 }
